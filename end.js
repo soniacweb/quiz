3c9e55e1d02293d3c707807745b23478793aa4a5
SoniaCWeb <sonia.choudhury@hotmail.co.uk>
2020-03-03 13:29:53 +0000

    local storage complete

--- end.js
+++ end.js
@@ -1,10 +1,20 @@
 const username = document.getElementById('username')
 const saveScoreBtn = document.getElementById('saveScoreBtn')
-const mostRecentSore = localStorage.getItem('mostRecentScore')
+const mostRecentScore = localStorage.getItem('mostRecentScore')
 const finalScore = document.getElementById('finalScore')
 
-finalScore.innerText = mostRecentSore
+//json stringify converts array into a string for localstorage
+//, JSON.stringify([])
+const highScores = JSON.parse(localStorage.getItem('highSores')) || []
 
+console.log(highScores)
+
+const MAX_HIGH_SCORES = 5
+
+//json parse converts back into an array
+// console.log(JSON.parse(localStorage.getItem('highScores')))
+
+finalScore.innerText = mostRecentScore
 
 username.addEventListener('keyup', () => {
   console.log(username.value)
@@ -14,5 +24,21 @@ username.addEventListener('keyup', () => {
 saveHighScore = (e) => {
   console.log('clicked save button')
   e.preventDefault()
+
+  const score = {
+    score: Math.floor(Math.random() * 100),
+    name: username.value
+  }
+  //add the score
+  highScores.push(score)
+  //sorting out the highscores- highest to lowest
+  highScores.sort( (a,b) => b.score - a.score)
+  //remove any scores after thr 5th highest
+  highScores.splice(5)
+  console.log(highScores)
+
+  //updating the local storage 
+  localStorage.setItem('highScores', JSON.stringify(highScores))
+  window.location.assign('/')
 }
 
